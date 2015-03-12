f0bc4c12a5cc370e914574bf22dc6b2925b93cf4
Brandon <Brandon@192.168.180.57>
2015-03-11 21:54:01 -0400

    Added an index page

--- Whiteboard/com/YCPCS/Main.java
+++ Whiteboard/com/YCPCS/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 		// Create and register a webapp context
 		WebAppContext handler = new WebAppContext();
-		handler.setContextPath("/lab03");
+		handler.setContextPath("/Whiteboard");
 		handler.setWar("./war"); // web app is in the war directory of the project
 		server.setHandler(handler);
 		
---
+++ Whiteboard/war/index.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Whiteboard</title>
+</head>
+
+<body>
+
+<center>
+<h1>Welcome To Whiteboard!</h1>
+<h3>This is a placeholder page!</h3>
+</center>
+</body>
+</html>
