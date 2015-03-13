46bf22cae2b7433ac164260bb218124078be2d26
Brandon <Brandon@192.168.180.203>
2015-03-12 20:36:38 -0400

    Stuff

---
+++ Whiteboard/com/YCPCS/JUnitTests/JUnitTests.java
@@ -0,0 +1,29 @@
+package com.YCPCS.JUnitTests;
+
+import com.YCPCS.User;
+
+import junit.framework.TestCase;
+
+public class JUnitTests extends TestCase{
+	User user = new User();
+	
+	public void testSetUsername(){
+		user.setUsername("User");
+		assertEquals("User", user.getUsername());
+	}
+	
+	public void testSetFirstName(){
+		user.setUsername("David");
+		assertEquals("David", user.getFirstname());
+	}
+	
+	public void testSetLastName(){
+		user.setUsername("Hoevemeyer");
+		assertEquals("Hoevemeyer", user.getLastname());
+	}
+	
+	public void testSetPassword(){
+		user.setUsername("Bojangle");
+		assertEquals("Bojangle", user.getPassword());
+	}
+}
