50a2f20f86ea2dd76fa2c06524644c0e596da2e0
Brandon Walton <bwalton1@ycp.edu>
2015-03-25 11:21:39 -0400

    Made changes to the web.xml to add the hub servlet

--- Whiteboard/src/com/YCPCS/Whiteboard/servlet/HubServlet.java
+++ Whiteboard/src/com/YCPCS/Whiteboard/servlet/HubServlet.java
@@ -7,32 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet{
+public class HubServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		super.doGet(req, resp);
 	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
 		super.doPost(req, resp);
-	}
-	
-	private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// Get parameters
-		String username = req.getParameter("username");
-		String password = req.getParameter("password");
-		
-		// Check whether parameters are valid
-		if (username == null || password == null) {
-			
-			return;
-		}
-		
-		//TODO: Use a controller to process the request
-		//TODO: Send back a response
-		//resp.setContentType("text/plain");
-		//resp.getWriter().println(result.toString());
 	}
 }
