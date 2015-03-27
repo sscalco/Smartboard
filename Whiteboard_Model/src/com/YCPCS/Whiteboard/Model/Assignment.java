aa5864d08b1255007cd25f4aca9d9c1d1a5d413d
HP <HP@192.168.150.176>
2015-03-26 20:02:37 -0400

    a

--- Whiteboard_Model/src/com/YCPCS/Whiteboard/Model/Assignment.java
+++ Whiteboard_Model/src/com/YCPCS/Whiteboard/Model/Assignment.java
@@ -1,5 +1,25 @@
 package com.YCPCS.Whiteboard.Model;
 
 public class Assignment {
+	private String assignmentName;
+	private float assignmentGrade;
 
+	
+	
+
+	public String getAssignmentName() {
+		return assignmentName;
+	}
+
+	public void setAssignmentName(String assignmentName) {
+		this.assignmentName = assignmentName;
+	}
+
+	public float getAssignmentGrade() {
+		return assignmentGrade;
+	}
+
+	public void setAssignmentGrade(float assignmentGrade) {
+		this.assignmentGrade = assignmentGrade;
+	}
 }
--- Whiteboard_Model/src/com/YCPCS/Whiteboard/Model/Class.java
+++ Whiteboard_Model/src/com/YCPCS/Whiteboard/Model/Class.java
@@ -1,5 +1,53 @@
 package com.YCPCS.Whiteboard.Model;
 
 public class Class {
+	private int classID;
+	private User classProfessor;
+	private Schedule classSchedule;
+	private Assignment classAssignment;
+	private Forum classForum;
+	private User classStudent;
 
+	
+	public int getClassID() {
+		return classID;
+	}
+	public void setClassID(int classID) {
+		this.classID = classID;
+	}
+	
+	public User getClassProfessor() {
+		return classProfessor;
+	}
+	public void setClassProfessor(User classProfessor) {
+		this.classProfessor = classProfessor;
+	}
+	
+	public Schedule getClassSchedule() {
+		return classSchedule;
+	}
+	public void setClassSchedule(Schedule classSchedule) {
+		this.classSchedule = classSchedule;
+	}
+	
+	public Assignment getClassAssignment() {
+		return classAssignment;
+	}
+	public void setClassAssignment(Assignment classAssignment) {
+		this.classAssignment = classAssignment;
+	}
+	
+	public Forum getClassForum() {
+		return classForum;
+	}
+	public void setClassForum(Forum classForum) {
+		this.classForum = classForum;
+	}
+	
+	public User getClassStudent() {
+		return classStudent;
+	}
+	public void setClassStudent(User classStudent) {
+		this.classStudent = classStudent;
+	}
 }
