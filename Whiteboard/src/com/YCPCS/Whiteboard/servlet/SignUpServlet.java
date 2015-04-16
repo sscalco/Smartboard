e1a7b61b9af3aca419ac6eebfbc54198699bb435
Brandon Walton <bfwalton@comcast.net>
2015-04-16 19:32:41 -0400

    Some changes

--- Whiteboard/src/com/YCPCS/Whiteboard/servlet/SignUpServlet.java
+++ Whiteboard/src/com/YCPCS/Whiteboard/servlet/SignUpServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.YCPCS.Whiteboard.Model.User;
+
 public class SignUpServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
@@ -19,7 +21,17 @@ public class SignUpServlet extends HttpServlet {
 	}
 	
 	private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/_view/signup.jsp").forward(req, resp);
+		String signup = (String) req.getParameter("signup");
+		if(signup!=null && signup.equals("true")){
+			System.out.println("Signing Up!");
+			User user = new User();
+			user.setFirstname((String) req.getParameter("firstname"));
+			user.setLastname((String) req.getParameter("lastname"));
+			user.setUsername((String) req.getParameter("username"));
+			user.setPassword((String) req.getParameter("password"));
+		}else{
+			req.getRequestDispatcher("/_view/signup.jsp").forward(req, resp);
+		}
 		return;
 	}
 }
