28f5bf627d0eaf524cb33bd6491249f8bbd75275
Brandon Walton <bfwalton@comcast.net>
2015-04-16 19:39:25 -0400

    Added the DerbyDatabase class

---
+++ Whiteboard_Persistence/src/com/YCPCS/Whiteboard/Database/DerbyDatabase.java
@@ -0,0 +1,54 @@
+package com.YCPCS.Whiteboard.Database;
+
+import java.util.List;
+
+import com.YCPCS.Whiteboard.Model.Lecture;
+import com.YCPCS.Whiteboard.Model.Relationship;
+import com.YCPCS.Whiteboard.Model.User;
+
+public class DerbyDatabase implements DatabaseLayer{
+
+	@Override
+	public int getUserIDByLogin(String Username, String password) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getFirstNameFromId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLastNameFromId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUserByUsernameAndPassword(String username, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUserToDatabase(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Lecture getClassById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Relationship> getRelationshipsByRootAndTarget(String root,
+			String target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
