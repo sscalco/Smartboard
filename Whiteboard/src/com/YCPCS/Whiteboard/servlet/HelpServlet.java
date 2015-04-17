6b8143e11dde82edcb5eb01aa8a370ac557e032d
HP <HP@192.168.150.122>
2015-04-16 21:23:08 -0400

    Servlets and JSP

---
+++ Whiteboard/src/com/YCPCS/Whiteboard/servlet/HelpServlet.java
@@ -0,0 +1,39 @@
+package com.YCPCS.Whiteboard.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.YCPCS.Whiteboard.Model.User;
+
+public class HelpServlet extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doRequest(req, resp);
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doRequest(req, resp);
+	}
+
+	protected void doRequest(HttpServletRequest req,
+			HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("HERE");
+
+		User user = (User) req.getSession().getAttribute("user");
+		if (user == null) {
+			// User is not logged in, redirect to login page
+			resp.sendRedirect(req.getContextPath() + "/login");
+			return;
+		} else {
+			req.setAttribute("username", user.getFirstname());
+		}
+		req.getRequestDispatcher("/_view/help.jsp").forward(req, resp);
+	}
+}
---
+++ Whiteboard/src/com/YCPCS/Whiteboard/servlet/UserAccountServlet.java
@@ -0,0 +1,44 @@
+package com.YCPCS.Whiteboard.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.YCPCS.Whiteboard.Model.User;
+
+public class UserAccountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+				throws ServletException, IOException {
+			doRequest(req, resp);
+		}
+
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+				throws ServletException, IOException {
+			doRequest(req, resp);
+		}
+
+		protected void doRequest(HttpServletRequest req,
+				HttpServletResponse resp) throws ServletException, IOException {
+			System.out.println("HERE");
+
+			User user = (User) req.getSession().getAttribute("user");
+			if (user == null) {
+				// User is not logged in, redirect to login page
+				resp.sendRedirect(req.getContextPath() + "/login");
+				return;
+			} else {
+				req.setAttribute("username", user.getFirstname());
+				user.setFirstname((String) req.getParameter("firstname"));
+				user.setLastname((String) req.getParameter("lastname"));
+				user.setUsername((String) req.getParameter("username"));
+				user.setPassword((String) req.getParameter("password"));
+				user.setEmail((String) req.getParameter("email"));
+			}
+			req.getRequestDispatcher("/_view/account.jsp").forward(req, resp);
+	}
+}
