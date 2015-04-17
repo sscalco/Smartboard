c75f35fd454c0242b59afb4266be60adc56d22dc
Brandon Walton <bfwalton@comcast.net>
2015-04-16 21:52:03 -0400

    Modified the create Class webpage

--- Whiteboard/src/com/YCPCS/Whiteboard/servlet/HubServlet.java
+++ Whiteboard/src/com/YCPCS/Whiteboard/servlet/HubServlet.java
@@ -9,49 +9,51 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.YCPCS.Whiteboard.Model.User;
 
-public class HubServlet extends HttpServlet{
+public class HubServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		doRequest(req, resp);
 	}
-	
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
 		doRequest(req, resp);
 	}
-	
-	private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		String doLogout = (String)req.getParameter("logout");
-		String doHelp = (String)req.getParameter("help");
-		String doAccount = (String)req.getParameter("account");
-		
-		System.out.println(doLogout +" "+ doHelp +" "+ doAccount);
-		
-		if(doLogout!=null && doLogout.equals("true")){
-			System.out.println("Logout is true: "+ doLogout);
-			resp.encodeRedirectURL("/login");
-		}else{
-			User user = (User) req.getSession().getAttribute("user");
-			if (user == null) {
-				// User is not logged in, redirect to login page
-				// FIXME: this would be better to do using a filter
-				resp.sendRedirect(req.getContextPath() + "/login");
-				return;
-			}else{
-				req.setAttribute("username", user.getFirstname());
-			}
-			
-		if(doHelp!=null && doHelp.equals("true")){
-			System.out.println("Help is true: "+ doHelp);
-			resp.sendRedirect(req.getContextPath() + "/UserAccount");
+
+	private void doRequest(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String doLogout = (String) req.getParameter("logout");
+		String doHelp = (String) req.getParameter("help");
+		String doAccount = (String) req.getParameter("account");
+
+		System.out.println(doLogout + " " + doHelp + " " + doAccount);
+
+		User user = (User) req.getSession().getAttribute("user");
+		if (user == null) {
+			// User is not logged in, redirect to login page
+			// FIXME: this would be better to do using a filter
+			resp.sendRedirect(req.getContextPath() + "/login");
 			return;
-		}else if(doAccount != null && doAccount.equals("true")){
-			System.out.println("Account Page is true: "+ doAccount);
-			resp.sendRedirect(req.getContextPath() + "/UserAccount");
-			return;
-		}else{
-			req.getRequestDispatcher("/_view/hub.jsp").forward(req, resp);
+		} else {
+			req.setAttribute("username", user.getFirstname());
 		}
+
+		if (doHelp != null && doHelp.equals("true")) {
+			//System.out.println("Help is true: " + doHelp);
+			resp.sendRedirect(req.getContextPath() + "/ClassCreate");
+			return;
+		} else if (doAccount != null && doAccount.equals("true")) {
+			//System.out.println("Account Page is true: " + doAccount);
+			resp.sendRedirect(req.getContextPath() + "/UserAccount");
+			return;
+		} else if (doLogout != null && doLogout.equals("true")) {
+			//System.out.println("DOING LOGOUT");
+			req.setAttribute("username", null);
+			resp.sendRedirect(req.getContextPath() + "/login");
+		} else {
+			req.getRequestDispatcher("/_view/hub.jsp").forward(req, resp);
 		}
 	}
 }
--- Whiteboard/war/_view/createClass.jsp
+++ Whiteboard/war/_view/createClass.jsp
@@ -8,20 +8,35 @@
 </head>
 <body>
 
-<div id="header">
-Create Class
-</div>
-
-<div id="data_entry">
-<select name="sometext" MULTIPLE size="5">
-  <option>text1</option>
-  <option>text2</option>
-  <option>text3</option>
-  <option>text4</option>
-  <option>text5</option>
-</select>
-</div>
-
 </body>
 
+</html>
+
+<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="_view/Account.css"/>
+	<link rel="stylesheet" type="text/css" href="_view/createClass.css"/>
+	<div id="Title">
+		<title>SmartBoard</title>
+	</div>
+	</head>
+<body>
+	<h1>Smartboard: Create Class</h1>
+	<div id="Options">
+		<p>Account | Help | Logout</p>
+	</div>
+	
+	<div id="form">
+		<div id="data_entry">
+		<select name="sometext" MULTIPLE size="5">
+  			<option>text1</option>
+  			<option>text2</option>
+  			<option>text3</option>
+  			<option>text4</option>
+  			<option>text5</option>
+		</select>
+</div>
+	</div>
+</body>	
+		
 </html>
